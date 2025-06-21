f14e63ceae4e623d1cdaf10eeb81b1fa00fec19e
Letícia Fernandes <leticiale2709@gmail.com>
2025-06-21 11:46:01 -0300

    Update user_data path in EC2 instance configuration and add tfplan to .gitignore

--- iac_aws_terraform/terraform/ec2.tf
+++ iac_aws_terraform/terraform/ec2.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "web_server" {
     ami           = data.aws_ami.amazon_linux.id
     instance_type = "t2.micro"
-    user_data     = base64encode(file("${path.module}/user_data.sh"))
+    user_data     = base64encode(file("${path.module}/../user_data.sh"))
     key_name      = aws_key_pair.ec2_key_pair.key_name
 
     # Associa os grupos de segurança criados anteriormente à instância
