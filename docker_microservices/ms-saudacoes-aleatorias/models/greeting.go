53c166cb7fc7dbe440395d9c0a71bd42730445b1
LeanDevLima <leanderson.devlima@gmail.com>
2025-07-25 15:30:51 -0300

    arrumando os arquivos

--- docker_microservices/ms-saudacoes-aleatorias/models/greeting.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "gorm.io/gorm"
-
-// Greeting representa a estrutura de um cumprimento no banco de dados.
-type Greeting struct {
-	gorm.Model        // Inclui campos como ID, CreatedAt, UpdatedAt, DeletedAt
-	Text       string `json:"text" binding:"required"` // O texto do cumprimento
-}
--- iac_aws_terraform/terraform/variable.tf
+++
@@ -1,7 +0,0 @@
-variable "my_public_ip" {
-    type        = string
-    description = "Public IP address for the Security Group SSH rule"
-    # IP está armazenado no arquivo terraform.tfvars
-}
-
-# Define as variáveis que serão utilizadas no Terraform para parametrizar a infraestrutura.
